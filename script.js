d08444be086ffc5415ec22bd6420b063239e9b90
project-kshatriya <projectkshatriyagit@gmail.com>
2024-05-09 22:50:43 +0530

    slideshow added

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-function main(){ 
+function main() {
+    // adding event listener to ham and close
+    const ham = document.getElementById("ham");
+    ham.addEventListener("click", () => {
+        document.querySelector(".nav-section-2").style.left = 0;
+    })
+    const close = document.getElementById("close");
+    close.addEventListener("click", () => {
+        document.querySelector(".nav-section-2").style.left = "-120%";
+    })
 
-const ham=document.getElementById("ham");
+    // Code for image slide
 
-console.log(ham)
-ham.addEventListener("click",()=>{
-    document.querySelector(".nav-section-2").style.left=0;
-    
-})
-const close=document.getElementById("close");
+    const left = document.getElementById("left-btn");
+    left.addEventListener("click", () => {
+        plusslide(-1);
+    })
+    const right = document.getElementById("right-btn");
+    right.addEventListener("click", () => {
+        plusslide(1)
+    })
+   
+    let slideindex=1;
+    showslide(slideindex);
 
+    function plusslide(n){
+        showslide(slideindex+=n)
+    }
 
-close.addEventListener("click",()=>{
-    document.querySelector(".nav-section-2").style.left= "-120%" ;
-    
-})
+    function showslide(n){
+        let i=0;
+        let slides=document.getElementsByClassName("slide")
+        if(n>slides.length){
+            slideindex=1;
+        }
+        if(n<1){slideindex=slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+          }
+          slides[slideindex-1].style.display="block";
+    }
+
 }
 
 
